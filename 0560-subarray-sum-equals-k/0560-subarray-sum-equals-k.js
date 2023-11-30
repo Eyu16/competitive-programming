a5961e364ec1b024f2a4542363582fe1835b0a8b
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2023-11-30 09:37:10 +0300

    Time: 76 ms (62.91%), Space: 49.6 MB (95.75%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -1,27 +1,19 @@
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number}
- */
-var subarraySum = function(nums, k) {
-    let sum = 0;
-    let count = 0;
-    let sumCountMap = new Map();
-    sumCountMap.set(0, 1);
-
-    for (let i = 0; i < nums.length; i++) {
-        sum += nums[i];
-
-        if (sumCountMap.has(sum - k)) {
-            count += sumCountMap.get(sum - k);
-        }
-
-        if (sumCountMap.has(sum)) {
-            sumCountMap.set(sum, sumCountMap.get(sum) + 1);
-        } else {
-            sumCountMap.set(sum, 1);
-        }
+var subarraySum = function (nums, k) {
+  let sum = 0;
+  let count = 0;
+  let sumCountMap = new Map();
+  sumCountMap.set(0, 1);
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    if (sumCountMap.has(sum - k)) {
+      count += sumCountMap.get(sum - k);
     }
-
-    return count;
+    if(sumCountMap.has(sum)){
+      sumCountMap.set(sum,sumCountMap.get(sum)+1);
+    }
+    else{
+      sumCountMap.set(sum,1);
+    }
+  }
+  return count;
 };

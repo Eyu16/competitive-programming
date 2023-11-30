64e86d193313cac90ab05a5ce3b8a1919535cbe1
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2023-11-30 09:26:32 +0300

    Time: 76 ms (62.91%), Space: 49.9 MB (86.25%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function(nums, k) {
+    let sum = 0;
+    let count = 0;
+    let sumCountMap = new Map();
+    sumCountMap.set(0, 1);
+
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+
+        if (sumCountMap.has(sum - k)) {
+            count += sumCountMap.get(sum - k);
+        }
+
+        if (sumCountMap.has(sum)) {
+            sumCountMap.set(sum, sumCountMap.get(sum) + 1);
+        } else {
+            sumCountMap.set(sum, 1);
+        }
+    }
+
+    return count;
+};

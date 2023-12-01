80485f67c2440a611fb94f444deb9cd64eb9c17e
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2023-12-01 09:54:16 +0300

    Time: 56 ms (34.65%), Space: 41.7 MB (77.81%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -71,21 +71,49 @@ var sortColors = function(nums) {
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var sortColors = function(nums) {
-    let i = 0; // Pointer for 0s
-    let j = 0; // Pointer for 1s
-    let k = nums.length - 1; // Pointer for 2s
+// var sortColors = function(nums) {
+//     let i = 0; // Pointer for 0s
+//     let j = 0; // Pointer for 1s
+//     let k = nums.length - 1; // Pointer for 2s
 
-    while (j <= k) {
-        if (nums[j] === 0) {
-            swap(nums, i, j);
+//     while (j <= k) {
+//         if (nums[j] === 0) {
+//             swap(nums, i, j);
+//             i++;
+//             j++;
+//         } else if (nums[j] === 1) {
+//             j++;
+//         } else {
+//             swap(nums, j, k);
+//             k--;
+//         }
+//     }
+// };
+
+// function swap(nums, i, j) {
+//     const temp = nums[i];
+//     nums[i] = nums[j];
+//     nums[j] = temp;
+// }
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    let low = 0;         // Pointer for 0s
+    let high = nums.length - 1;  // Pointer for 2s
+    let i = 0;           // Current position
+
+    while (i <= high) {
+        if (nums[i] === 0) {
+            swap(nums, i, low);
             i++;
-            j++;
-        } else if (nums[j] === 1) {
-            j++;
+            low++;
+        } else if (nums[i] === 2) {
+            swap(nums, i, high);
+            high--;
         } else {
-            swap(nums, j, k);
-            k--;
+            i++;
         }
     }
 };

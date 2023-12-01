f2df27f0136887f65e3f8bce040fbc09ab1f4ca5
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2023-12-01 09:51:03 +0300

    Time: 60 ms (16.13%), Space: 42.1 MB (38.58%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -1,20 +1,98 @@
+// /**
+//  * @param {number[]} nums
+//  * @return {void} Do not return anything, modify nums in-place instead.
+//  */
+// var sortColors = function(nums) {
+// // let temp;
+// //     for(let i = 0 ; i < nums.length ; i++){
+// //         for(let j = i+1 ;j>=0;j--){
+// //             if(nums[j-1]>nums[j]){
+// //                 temp = nums[j-1];
+// //                 nums[j-1] = nums[j];
+// //                 nums[j] = temp
+// //             }
+// //             else{
+// //                 break;
+// //             }
+// //         }
+// //     }
+// //     return nums;
+//     let temp;
+//     let i = 0;
+//     let k = 0;
+//     let j = nums.length - 1;
+//     let ans = [];
+//      while(i<j){
+//          if(nums[i] === 2){
+//              if(ans[j+1] == 1){
+//                  ans[j+1] = 2;
+//                  ans[j] = 1;
+//                  i++;
+//                  j--;
+//              }
+//          }
+//        else if(nums[i] === 1){
+//            ans[j]=1;
+//            j--;
+//            i++;
+//        }
+//            else{
+//                ans[k] = 0
+//                i++;
+//                k++;
+//            }
+//      }
+//     return ans;
+// };
 /**
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-let temp;
-    for(let i = 0 ; i < nums.length ; i++){
-        for(let j = i+1 ;j>=0;j--){
-            if(nums[j-1]>nums[j]){
-                temp = nums[j-1];
-                nums[j-1] = nums[j];
-                nums[j] = temp
-            }
-            else{
-                break;
-            }
+    let i = 0; // Pointer for 0s
+    let j = 0; // Pointer for 1s
+    let k = nums.length - 1; // Pointer for 2s
+
+    while (j <= k) {
+        if (nums[j] === 0) {
+            swap(nums, i, j);
+            i++;
+            j++;
+        } else if (nums[j] === 1) {
+            j++;
+        } else {
+            swap(nums, j, k);
+            k--;
         }
     }
-    return nums;
 };
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    let i = 0; // Pointer for 0s
+    let j = 0; // Pointer for 1s
+    let k = nums.length - 1; // Pointer for 2s
+
+    while (j <= k) {
+        if (nums[j] === 0) {
+            swap(nums, i, j);
+            i++;
+            j++;
+        } else if (nums[j] === 1) {
+            j++;
+        } else {
+            swap(nums, j, k);
+            k--;
+        }
+    }
+};
+
+function swap(nums, i, j) {
+    const temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
+

dc29ccc26c0e14dcbb160ded8bc393b53eaf9baf
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2023-12-09 14:18:06 +0300

    Time: 52 ms (57.34%), Space: 42.4 MB (43.64%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.js
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -20,19 +20,15 @@
  * @return {string}
  */
 var mergeAlternately = function(word1, word2) {
-    let merged = '';
-    let i = 0;
-    let j = 0;
-
-    while (i < word1.length && j < word2.length) {
-        merged += word1[i] + word2[j];
-        i++;
+ let merged = '';
+ let j = 0;
+ let k = 0;
+    while(j<word1.length && k<word2.length){
+        merged += word1[j]+word2[k];
         j++;
+        k++;
     }
-
-    // Append the remaining characters from the non-empty string
-    merged += word1.slice(i) + word2.slice(j);
-
+    merged += word1.slice(j)+word2.slice(k);
     return merged;
 };
 

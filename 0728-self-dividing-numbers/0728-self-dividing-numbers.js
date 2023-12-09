6e769984bc72d51efedc4260b7cb4f16532b9071
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2023-12-09 08:51:05 +0300

    Time: 56 ms (75.63%), Space: 42.8 MB (76.02%) - LeetHub

--- 0728-self-dividing-numbers/0728-self-dividing-numbers.js
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.js
@@ -1,18 +1,64 @@
+// /**
+//  * @param {number} left
+//  * @param {number} right
+//  * @return {number[]}
+//  */
+var selfDividingNumbers = function(left, right) {
+    let arr = [];
+    // for(let i = left ; i<=right ; i++){
+    //     if(i%10 == 0) continue;
+    //    let strNum  = i;
+    //     strNum = String(strNum);
+    //     for(let k = 0; k < strNum.length ; k++){
+    //         if(i%(+strNum[k]) !== 0) break;
+    //         if(i%(+strNum[k]) == 0 && k == strNum.length-1) arr.push(i)
+    //     }
+    // }
+    for(let i = left ; i <= right ; i++){
+        if(i%10 == 0) continue;
+        let isSelefDividing = true;
+        let num = i;
+        while(num > 0){
+            const digit = num%10;
+            if(i%digit !== 0){
+                isSelefDividing = false;
+                break;
+            }
+            num = Math.floor(num/10);
+        }
+        if(isSelefDividing) arr.push(i);
+    }
+    return arr;
+};
 /**
  * @param {number} left
  * @param {number} right
  * @return {number[]}
  */
-var selfDividingNumbers = function(left, right) {
-    let arr = [];
-    for(let i = left ; i<=right ; i++){
-        if(i%10 == 0) continue;
-       let strNum  = i;
-        strNum = String(strNum);
-        for(let k = 0; k < strNum.length ; k++){
-            if(i%(+strNum[k]) !== 0) break;
-            if(i%(+strNum[k]) == 0 && k == strNum.length-1) arr.push(i)
-        }
-    }
-    return arr;
-};
+// var selfDividingNumbers = function(left, right) {
+//     let arr = [];
+    
+//     for (let i = left; i <= right; i++) {
+//         if (i % 10 === 0) continue; // Skip numbers ending in 0
+        
+//         let isSelfDividing = true;
+//         let num = i;
+        
+//         while (num > 0) {
+//             const digit = num % 10;
+            
+//             if (i % digit !== 0) {
+//                 isSelfDividing = false;
+//                 break;
+//             }
+            
+//             num = Math.floor(num / 10); // Move to the next digit
+//         }
+        
+//         if (isSelfDividing) {
+//             arr.push(i);
+//         }
+//     }
+    
+//     return arr;
+// };

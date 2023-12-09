16b75079d256d547c6ef8b372d83ea56535478c0
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2023-12-09 16:34:01 +0300

    Time: 55 ms (30.66%), Space: 41.5 MB (84.44%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.js
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.js
@@ -3,14 +3,20 @@
  * @return {number}
  */
 var minStartValue = function(nums) {
-    
-    let startValue = 1;
+    // let startValue = 1;
+    // let curSum = 0;
+    // let minSum = 0;
+    // for(let i = 0;i<nums.length;i++){
+    //   curSum += nums[i];
+    //     minSum = Math.min(minSum,curSum);
+    // }
+    // return startValue - minSum; 
     let curSum = 0;
     let minSum = 0;
+    let startValue = 1;
     for(let i = 0;i<nums.length;i++){
-      curSum += nums[i];
-        minSum = Math.min(minSum,curSum);
+        curSum += nums[i];
+        minSum = Math.min(curSum,minSum);
     }
     return startValue - minSum;
-    
 };

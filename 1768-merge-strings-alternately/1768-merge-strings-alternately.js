9b7133c44c24bd16c6206290738058b438205f6c
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2023-12-09 14:14:45 +0300

    Time: 49 ms (74.23%), Space: 41.5 MB (97.68%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.js
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -1,16 +1,38 @@
+// /**
+//  * @param {string} word1
+//  * @param {string} word2
+//  * @return {string}
+//  */
+// var mergeAlternately = function(word1, word2) {
+//     let str='';
+//     size = word1.length > word2.length?word1.length:word2.length;
+//     for(let i = 0 ; i<size ; i++){
+//         if(word1[i])
+//      str+=word1[i];
+//      if(word2[i])
+//      str+=word2[i];
+//     }
+//     return str;
+// };
 /**
  * @param {string} word1
  * @param {string} word2
  * @return {string}
  */
 var mergeAlternately = function(word1, word2) {
-    let str='';
-    size = word1.length > word2.length?word1.length:word2.length;
-    for(let i = 0 ; i<size ; i++){
-        if(word1[i])
-     str+=word1[i];
-     if(word2[i])
-     str+=word2[i];
+    let merged = '';
+    let i = 0;
+    let j = 0;
+
+    while (i < word1.length && j < word2.length) {
+        merged += word1[i] + word2[j];
+        i++;
+        j++;
     }
-    return str;
+
+    // Append the remaining characters from the non-empty string
+    merged += word1.slice(i) + word2.slice(j);
+
+    return merged;
 };
+

86d24e85a05b07890658a9374d93ddbed1b47fa2
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2023-12-11 10:39:36 +0300

    Time: 45 ms (97.28%), Space: 41.9 MB (94.95%) - LeetHub

--- 0728-self-dividing-numbers/0728-self-dividing-numbers.js
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.js
@@ -3,33 +3,33 @@
 //  * @param {number} right
 //  * @return {number[]}
 //  */
-var selfDividingNumbers = function(left, right) {
-    let arr = [];
-    // for(let i = left ; i<=right ; i++){
-    //     if(i%10 == 0) continue;
-    //    let strNum  = i;
-    //     strNum = String(strNum);
-    //     for(let k = 0; k < strNum.length ; k++){
-    //         if(i%(+strNum[k]) !== 0) break;
-    //         if(i%(+strNum[k]) == 0 && k == strNum.length-1) arr.push(i)
-    //     }
-    // }
-    for(let i = left ; i <= right ; i++){
-        if(i%10 == 0) continue;
-        let isSelefDividing = true;
-        let num = i;
-        while(num > 0){
-            const digit = num%10;
-            if(i%digit !== 0){
-                isSelefDividing = false;
-                break;
-            }
-            num = Math.floor(num/10);
-        }
-        if(isSelefDividing) arr.push(i);
-    }
-    return arr;
-};
+// var selfDividingNumbers = function(left, right) {
+//     let arr = [];
+//     // for(let i = left ; i<=right ; i++){
+//     //     if(i%10 == 0) continue;
+//     //    let strNum  = i;
+//     //     strNum = String(strNum);
+//     //     for(let k = 0; k < strNum.length ; k++){
+//     //         if(i%(+strNum[k]) !== 0) break;
+//     //         if(i%(+strNum[k]) == 0 && k == strNum.length-1) arr.push(i)
+//     //     }
+//     // }
+//     for(let i = left ; i <= right ; i++){
+//         if(i%10 == 0) continue;
+//         let isSelefDividing = true;
+//         let num = i;
+//         while(num > 0){
+//             const digit = num%10;
+//             if(i%digit !== 0){
+//                 isSelefDividing = false;
+//                 break;
+//             }
+//             num = Math.floor(num/10);
+//         }
+//         if(isSelefDividing) arr.push(i);
+//     }
+//     return arr;
+// };
 /**
  * @param {number} left
  * @param {number} right
@@ -63,21 +63,38 @@ var selfDividingNumbers = function(left, right) {
 //     return arr;
 // };
 var selfDividingNumbers = function (left, right) {
-  let arr = [];
-  for(let i = left ; i<=right;i++){
-    if(i%10 == 0) continue;
-    let num = i;
-    let isSelefD = true;
-    while(num>0){
-      let digit =  num%10;
-      if(i%digit !== 0){
-        isSelefD = false;
-        break;
-      }
-      num = Math.floor(num/10);
+//   let arr = [];
+//   for(let i = left ; i<=right;i++){
+//     if(i%10 == 0) continue;
+//     let num = i;
+//     let isSelefD = true;
+//     while(num>0){
+//       let digit =  num%10;
+//       if(i%digit !== 0){
+//         isSelefD = false;
+//         break;
+//       }
+//       num = Math.floor(num/10);
+//     }
+// if(isSelefD) 
+// arr.push(i);
+//   }
+// return arr;
+    let arr = [];
+    for(let i = left ; i <= right ; i++ ){
+        if(i%10 === 0) continue;
+        let num = i;
+        let isSelefD = true;
+        while(num > 0){
+            let digit = num % 10;
+            if(i % digit !== 0){
+                isSelefD = false;
+                break;
+            }
+            else num = Math.floor(num/10);
+        }
+        if (isSelefD)
+            arr.push(i);
     }
-if(isSelefD) 
-arr.push(i);
-  }
-return arr;
+    return arr;
 };

7a4e0244cc4d3e7a1998492aa6a0314085333492
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2023-12-11 10:27:21 +0300

    Time: 78 ms (38.79%), Space: 46.7 MB (51.72%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.js
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.js
@@ -1,42 +1,42 @@
 var subarraysDivByK = function(nums, k) {
+
     let count = 0;
     let remainder = 0;
     let remainderMap = new Map();
-    remainderMap.set(0, 1);
-
-    for (let i = 0; i < nums.length; i++) {
-      remainder = (remainder + nums[i]) % k;
-        if (remainder < 0) {
-            // Handle negative remainders
+    remainderMap.set(0,1);
+    for(let i = 0; i < nums.length;i++){
+        remainder = (remainder + nums[i]) % k;
+        if(remainder < 0)
             remainder += k;
+            if(remainderMap.has(remainder)){
+                count += remainderMap.get(remainder);
+                remainderMap.set(remainder, remainderMap.get(remainder)+1);
+            }
+            else{
+                remainderMap.set(remainder,1);
+            }
         }
-        if (remainderMap.has(remainder)) {
-            count += remainderMap.get(remainder);
-            remainderMap.set(remainder, remainderMap.get(remainder) + 1);
-        }
-
-       else {
-            remainderMap.set(remainder, 1);
-        }
-    }
-
     return count;
-    // let count = 0;
-    // let remainder = 0;
-    // let remainderMap = new Map();
-    // remainderMap.set(0,1);
-    // for(let i = 0; i < nums.length;i++){
-    //     remainder = (remainder + nums[i]) % k;
-    //     if(remainder < 0){
-    //         remainder += k;
-    //         if(remainderMap.has(remainder)){
-    //             count += remainderMap.get(remainder);
-    //             remainderMap.set(remainder,remainderMap.get(remainder)+1);
-    //         }
-    //         else{
-    //             remainderMap.set(remainder,1);
-    //         }
-    //     }
-    // }
-    // return count;
 };
+//     let count = 0;
+//     let remainder = 0;
+//     let remainderMap = new Map();
+//     remainderMap.set(0, 1);
+
+//     for (let i = 0; i < nums.length; i++) {
+//       remainder = (remainder + nums[i]) % k;
+//         if (remainder < 0) {
+//             // Handle negative remainders
+//             remainder += k;
+//         }
+//         if (remainderMap.has(remainder)) {
+//             count += remainderMap.get(remainder);
+//             remainderMap.set(remainder, remainderMap.get(remainder) + 1);
+//         }
+
+//        else {
+//             remainderMap.set(remainder, 1);
+//         }
+//     }
+
+//     return count;

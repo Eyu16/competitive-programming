dcfef99fcb83ca2f0262d5414336a529a1976bfb
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2023-12-11 08:02:27 +0300

    Time: 58 ms (92.65%), Space: 44.5 MB (34.19%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
@@ -5,20 +5,32 @@
 var smallerNumbersThanCurrent = function(nums) {
   let max = Math.max(...nums)+1;
     let smaller = new Array(max).fill(0);
-    let sum = 0;
-    for(let i = 0; i<nums.length;i++){
+//     for(let i = 0; i<nums.length;i++){
+//         smaller[nums[i]]++;
+//     }
+//     for(let i = 1 ; i < smaller.length ; i++){
+//         smaller[i] += smaller[i-1]
+       
+//     }
+//     for(let i = 0 ; i<nums.length ; i++){
+//         let position = nums[i];
+//         if(position === 0) nums[i] = 0;
+//         else{
+//             nums[i] = smaller[position-1];
+            
+//         }
+//     }
+    for(let i = 0 ; i < nums.length ; i++){
         smaller[nums[i]]++;
     }
     for(let i = 1 ; i < smaller.length ; i++){
-        smaller[i] += smaller[i-1]
-       
+        smaller[i] += smaller[i-1];
     }
-    for(let i = 0 ; i<nums.length ; i++){
-        let position = nums[i];
-        if(position === 0) nums[i] = 0;
+    for(let i = 0 ; i < nums.length ; i++){
+        let pos = nums[i];
+        if(pos === 0) nums[i] = 0;
         else{
-            nums[i] = smaller[position-1];
-            
+            nums[i] = smaller[pos-1];
         }
     }
     return nums;

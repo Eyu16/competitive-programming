4e3e474a5505f0f8b8956198ed943d9f6df134e9
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2023-12-11 06:05:36 +0300

    Time: 71 ms (62.56%), Space: 51.9 MB (19.84%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -5,9 +5,22 @@
 var maxProfit = function(prices) {
     let minPrice = prices[0];
     let maxProfit = 0;
-    for(let i = 1 ; i<prices.length;i++){
-        minPrice = Math.min(minPrice,prices[i]);
-        maxProfit = Math.max(maxProfit,prices[i]-minPrice);
+    let left = 0;
+    let right = 1;
+    let profit = 0;
+    // for(let i = 1 ; i<prices.length;i++){
+    //     minPrice = Math.min(minPrice,prices[i]);
+    //     maxProfit = Math.max(maxProfit,prices[i]-minPrice);
+    // }
+    while(right <prices.length){
+        if(prices[left]<prices[right]){
+        profit = prices[right] - prices[left];
+        maxProfit = Math.max(profit,maxProfit);
+        }
+        else{
+            left = right;
+        }
+        right++;
     }
     return maxProfit;
 };

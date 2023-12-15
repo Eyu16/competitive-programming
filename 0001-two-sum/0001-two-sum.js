df74faeea3091674e31767d6afef30bfaaacb58f
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2023-12-15 05:57:52 +0300

    Time: 78 ms (46.63%), Space: 45.9 MB (5.32%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,15 +4,17 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let index = [];
-    for(let i = 0 ; i < nums.length - 1 ;i++){
-        for(let j = i+1 ; j < nums.length ; j++){
-            if((nums[i] + nums[j]) == target){
-                index.push(i);
-                index.push(j);
-                break;
-            }
-        }
+    let num1 = [...nums];
+    num1.sort((a,b)=>a-b);
+    console.log(nums)
+    let i = 0 ;
+    let j = num1.length - 1;
+    while(i < j){
+        if(num1[i]+num1[j] === target)
+            return [nums.indexOf(num1[i]),nums.lastIndexOf(num1[j])];
+        else if(num1[i]+num1[j] > target)
+            j--;
+        else i++;
     }
-    return index;
+    
 };

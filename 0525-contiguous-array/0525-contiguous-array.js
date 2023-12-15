e96cadb01088309a168f01805f93654aac22b0cd
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2023-12-15 09:59:55 +0300

    Time: 82 ms (82.13%), Space: 49.3 MB (91.91%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.js
+++ 0525-contiguous-array/0525-contiguous-array.js
@@ -1,20 +1,17 @@
 var findMaxLength = function(nums) {
-    const sumMap = new Map();
-    let maxLen = 0;
+    let maxLength = 0;
     let sum = 0;
-
-    // Initialize sumMap with an entry for 0 and its index as -1
-    sumMap.set(0, -1);
-
-    for (let i = 0; i < nums.length; i++) {
-        sum += nums[i] === 0 ? -1 : 1;
-
-        if (sumMap.has(sum)) {
-            maxLen = Math.max(maxLen, i - sumMap.get(sum));
-        } else {
-            sumMap.set(sum, i);
+    let sumMap = new Map();
+    sumMap.set(0,-1);
+    for(let i = 0 ; i < nums.length ; i++){
+        sum += nums[i] === 0? -1:1;
+        if(sumMap.has(sum)){
+            maxLength = Math.max(maxLength,i-sumMap.get(sum));
+        }
+        else{
+            sumMap.set(sum,i);
         }
     }
+    return maxLength;
+}
 
-    return maxLen;
-};

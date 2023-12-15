ab14311ccfa97d806855d6f52672cc17b2448836
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2023-12-15 08:27:58 +0300

    Time: 79 ms (38.52%), Space: 46.7 MB (61.48%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.js
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.js
@@ -1,42 +1,20 @@
 var subarraysDivByK = function(nums, k) {
-
     let count = 0;
-    let remainder = 0;
+    let sumRemainder = 0;
     let remainderMap = new Map();
     remainderMap.set(0,1);
-    for(let i = 0; i < nums.length;i++){
-        remainder = (remainder + nums[i]) % k;
-        if(remainder < 0)
-            remainder += k;
-            if(remainderMap.has(remainder)){
-                count += remainderMap.get(remainder);
-                remainderMap.set(remainder, remainderMap.get(remainder)+1);
-            }
-            else{
-                remainderMap.set(remainder,1);
-            }
+    for(let i = 0 ; i < nums.length ; i++){
+        sumRemainder = (sumRemainder + nums[i])%k
+        if(sumRemainder < 0)
+            sumRemainder += k;
+        if(remainderMap.has(sumRemainder)){
+            count += remainderMap.get(sumRemainder);
+          remainderMap.set(sumRemainder,remainderMap.get(sumRemainder)+1);
         }
+        else{
+            remainderMap.set(sumRemainder,1);
+        }
+    }
+
     return count;
 };
-//     let count = 0;
-//     let remainder = 0;
-//     let remainderMap = new Map();
-//     remainderMap.set(0, 1);
-
-//     for (let i = 0; i < nums.length; i++) {
-//       remainder = (remainder + nums[i]) % k;
-//         if (remainder < 0) {
-//             // Handle negative remainders
-//             remainder += k;
-//         }
-//         if (remainderMap.has(remainder)) {
-//             count += remainderMap.get(remainder);
-//             remainderMap.set(remainder, remainderMap.get(remainder) + 1);
-//         }
-
-//        else {
-//             remainderMap.set(remainder, 1);
-//         }
-//     }
-
-//     return count;

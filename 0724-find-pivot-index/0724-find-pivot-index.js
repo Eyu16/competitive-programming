cdd32f6e3792c9f9844dcfc19cb3c7e6efc1f234
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2023-12-15 07:31:58 +0300

    Time: 68 ms (45.18%), Space: 44.1 MB (80.48%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.js
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -3,22 +3,14 @@
  * @return {number}
  */
 var pivotIndex = function(nums) {
-    // let leftSum = 0;
-    // let totalSum = nums.reduce((acc,cur)=>acc+cur,0);
-    // for(let i = 0 ; i < nums.length ; i++){
-    //     totalSum -= nums[i];
-    //     if(leftSum == totalSum)
-    //         return i;
-    //     leftSum += nums[i];
-    // }
-    // return -1;
-    let leftSum = 0;
     let totalSum = nums.reduce((acc,cur)=>acc+cur,0);
+    let leftSum = 0;
     for(let i = 0 ; i < nums.length ; i++){
         totalSum -= nums[i];
         if(totalSum === leftSum)
             return i;
-        leftSum += nums[i];
+            leftSum += nums[i]
     }
     return -1;
+    
 };

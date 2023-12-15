4a30571609877b6f827e84499cbb40e0a19c8154
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2023-12-15 07:45:38 +0300

    Time: 66 ms (23.33%), Space: 42.3 MB (41.11%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.js
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.js
@@ -3,23 +3,12 @@
  * @return {number}
  */
 var pivotInteger = function(n) {
-//     const totalSum = (n * (n + 1)) / 2;
-//     let leftSum = 0;
-
-//     for (let i = 1; i <= n; i++) {
-//         if (leftSum === totalSum - leftSum - i) {
-//             return i;
-//         }
-
-//         leftSum += i;
-//     }
-
-//     return -1;
     let totalSum = (n*(n+1))/2;
     let leftSum = 0;
     for(let i = 1; i <= n ; i++){
-        if(leftSum === totalSum - leftSum -i)
-        return i;
+       totalSum -= i;
+        if(totalSum === leftSum)
+            return i
         leftSum+=i;
     }
      

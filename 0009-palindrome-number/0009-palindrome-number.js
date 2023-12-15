fef794a8aa4702d0e1ee6eecc1d4914780a11f52
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2023-12-15 06:25:45 +0300

    Time: 125 ms (90.33%), Space: 51 MB (50.98%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,18 +3,31 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    if(x<0) return false;
-let strNum = String(x);
-let start = 0;
-let end = strNum.length-1;
-    while(start<end){{
-        if(strNum[start] !== strNum[end]){
-            return false;
-        }
-        start++;
-        end--;
-    }
+    // if(x<0) return false;
+// let strNum = String(x);
+// let start = 0;
+// let end = strNum.length-1;
+//     while(start<end){{
+//         if(strNum[start] !== strNum[end]){
+//             return false;
+//         }
+//         start++;
+//         end--;
+//     }
         
-    }
+    // }
+    // return true;
+    if(x<0) return false;
+    x = String(x);
+    let start = 0;
+    let end = x.length - 1;
+    while(start < end){
+        if(x[start] !== x[end]){
+               return false;
+        }
+        end--;
+        start++;
+        }
+     
     return true;
 };

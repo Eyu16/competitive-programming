540247c9e2931036f6649822c4d058e1fd58f46b
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2023-12-16 09:40:16 +0300

    Time: 75 ms (47.85%), Space: 42.8 MB (81.18%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -5,25 +5,13 @@
  */
 var isAnagram = function(s, t) {
     if( s.length !== t.length) return false;
-    let sMap = new Map();
-    let tMap = new Map();
+    let charMap = new Map();
     for(let i = 0 ; i < s.length ; i++){
-        if (sMap.has(s[i])){
-            sMap.set(s[i],sMap.get(s[i])+1);
-        }
-        else{
-            sMap.set(s[i],1);
-        }
-        
-        if (tMap.has(t[i])){
-            tMap.set(t[i],tMap.get(t[i])+1);
-        }
-        else{
-            tMap.set(t[i],1);
-        }
+    charMap.set(s[i],(charMap.get(s[i])||0)+1);
+    charMap.set(t[i],(charMap.get(t[i])||0)-1);
     }
-    for(let i of sMap.keys()){
-        if(sMap.get(i) !== tMap.get(i))
+    for(let count  of charMap.values()){
+        if(count !== 0)
             return false;
     }
   return true;

02e21ce51e76e199d969e02f9d38bcfa2c729871
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2023-12-16 09:03:01 +0300

    Time: 83 ms (49.49%), Space: 53 MB (74.66%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,17 +3,23 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    const occurMap = new Map();
+    // const occurMap = new Map();
+    // for(let i = 0 ; i < nums.length ; i++){
+    //     if(occurMap.has(nums[i])){
+    //         occurMap.set(nums[i],occurMap.get(nums[i])+1);
+    //         if(occurMap.get(nums[i]) > 1){
+    //             return true;
+    //         }
+    //     }
+    //     else{
+    //         occurMap.set(nums[i],1);
+    //     } 
+    // }
+    const numsSet = new Set();
     for(let i = 0 ; i < nums.length ; i++){
-        if(occurMap.has(nums[i])){
-            occurMap.set(nums[i],occurMap.get(nums[i])+1);
-            if(occurMap.get(nums[i]) > 1){
-                return true;
-            }
-        }
-        else{
-            occurMap.set(nums[i],1);
-        } 
+        if(numsSet.has(nums[i]))
+            return true;
+        numsSet.add(nums[i]);
     }
     return false;
 };

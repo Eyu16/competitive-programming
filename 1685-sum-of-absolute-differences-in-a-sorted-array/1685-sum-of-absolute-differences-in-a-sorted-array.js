31e7e52aa8a7e054cf99395768471b6bca617327
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2023-12-16 08:49:16 +0300

    Time: 181 ms (50.58%), Space: 67.7 MB (88.96%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.js
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.js
@@ -3,22 +3,14 @@
  * @return {number[]}
  */
 var getSumAbsoluteDifferences = function(nums) {
-let cumulativeSum = [0];
+    let totalSum = nums.reduce((acc,cur) => acc+cur,0);
+    let leftSum = 0;
     let ans = [];
     for(let i = 0 ; i < nums.length ; i++){
-        cumulativeSum[i+1] = cumulativeSum[i] + nums[i];
-    }
-    cumulativeSum = cumulativeSum.slice(1);
-    for(i = 0 ; i < nums.length ; i++){
-        if( i === 0 || i === nums.length - 1 ){
-            ans.push(Math.abs(cumulativeSum[nums.length-1] -( nums[i] * nums.length)))
-        }
-        else{
-            let leftSum = cumulativeSum[i-1];
-            let rightSum = cumulativeSum[nums.length - 1] - cumulativeSum[i];
-            let sum = Math.abs(leftSum - (i * nums[i])) + Math.abs(rightSum - ((nums.length - 1 - i) * nums[i] ))
-            ans.push(sum);
-        }
+        let rightSum = totalSum - nums[i] - leftSum;
+        ans.push( i * nums[i] - leftSum + (rightSum - ((nums.length - 1 - i) * nums[i])))
+        
+            leftSum += nums[i]
     }
     return ans;
 };

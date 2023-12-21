5c2b470c7b29770d25026441e93d12d2de5a9fc0
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2023-12-21 07:12:36 +0300

    Time: 93 ms (29.92%), Space: 48.2 MB (89.59%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -5,22 +5,18 @@
 var sortedSquares = function(nums) {
     let i = 0;
     let sortedSquared = []
-    
     let j= nums.length - 1
     let x = j
     while(i <= j){
         if(Math.abs(nums[i])>Math.abs(nums[j])){
         sortedSquared[x] = nums[i] * nums[i];
             i++;
-            x--;
-        
         }
         else{
             sortedSquared[x] = nums[j] * nums[j];
             j--;
-            x--;
-          
         }
+        x--;
     }
     return sortedSquared;
 };

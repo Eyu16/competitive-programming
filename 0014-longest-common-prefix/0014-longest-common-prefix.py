abe6255090deb964d5a006b8b3855b3abce54a58
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2024-02-02 16:19:36 +0300

    Time: 39 ms (62.09%), Space: 16.7 MB (61.99%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -3,8 +3,6 @@ class Solution:
         strs.sort()
         result = ""
         max_check = len(strs[0])
-        for word in strs:
-            max_check = len(word) if max_check > len(word) else max_check
         for i in range(max_check):
             if strs[0][i] == strs[-1][i]:
                 result += strs[0][i]

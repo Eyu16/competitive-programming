a30a201c5e54d4f0e7f8564e45dd3cc658d74e4c
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2024-02-05 17:52:03 +0300

    Time: 117 ms (53.15%), Space: 19 MB (92.57%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,12 +1,16 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        frequency_map = {}
+        result = 0
         for num in nums:
-            if num in frequency_map:
-                frequency_map[num] += 1
-            else:
-                frequency_map[num] = 1
-        for key in frequency_map:
-            if frequency_map[key] == 1:
-                return key
+            result ^= num
+        return result
+        # frequency_map = {}
+        # for num in nums:
+        #     if num in frequency_map:
+        #         frequency_map[num] += 1
+        #     else:
+        #         frequency_map[num] = 1
+        # for key in frequency_map:
+        #     if frequency_map[key] == 1:
+        #         return key
         

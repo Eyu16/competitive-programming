cfb5a347e2f7cb3a802af574827965fa0e5edabc
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2024-02-05 23:31:39 -0800

    Create list-comprehension.py

---
+++ list-comprehension.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    i = [x for x in range(x + 1)]
+    j = [y for y in range(y + 1)]
+    k = [z for z in range(z + 1)]
+    print ([[a, b, c] for a in i for b in j for c in k if sum([a, b, c]) != n])

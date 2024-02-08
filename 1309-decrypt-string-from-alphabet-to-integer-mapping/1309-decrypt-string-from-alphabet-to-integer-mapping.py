4e06c34bd980293eaeb186f656c00f79f89d90b5
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2024-02-08 11:02:18 +0300

    Time: 35 ms (70.43%), Space: 16.4 MB (86.32%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
@@ -1,26 +1,22 @@
 class Solution:
     def freqAlphabets(self, s: str) -> str: 
         combinations = {
-        '1': 'a','2': 'b','3': 'c','4': 'd','5': 'e','6': 'f','7': 'g','8': 'h','9': 'i','10': 'j','11': 'k','12': 'l','13': 'm','14': 'n','15': 'o','16': 'p','17': 'q','18': 'r','19': 's','20': 't','21': 'u','22': 'v','23': 'w','24': 'x','25': 'y','26': 'z',
+        '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f', '7': 'g', '8': 'h', '9': 'i',
+        '10#': 'j', '11#': 'k', '12#': 'l', '13#': 'm', '14#': 'n', '15#': 'o', '16#': 'p',
+        '17#': 'q', '18#': 'r', '19#': 's', '20#': 't', '21#': 'u', '22#': 'v', '23#': 'w',
+        '24#': 'x', '25#': 'y', '26#': 'z',
     }
-        digits = s.split("#")
-        string = ""
-        if '#' in s:
-            for digit in digits:
-                if digit != '':
-                    if len(digit) > 2:
-                        for i in range(len(digit)-2):
-                            string += combinations[digit[i]]
-                        string += combinations[digit[-2:]]
-                    else:
-                        if digit == s[-2:]:
-                            for char in digit:
-                                string += combinations[char]
-                        else:
-                            string += combinations[digit]
-        else:
-            for char in digits[0]:
-                string += combinations[char]
+        result = []
+        i = len(s) - 1
+        while i >= 0:
+            if s[i] == '#':
+                key = s[i-2:i+1]
+                result.append(combinations[key])
+                i -= 3
+            else:
+                result.append(combinations[s[i]])
+                i -= 1
 
-        return string
+        return "".join((result[::-1]))
+
         

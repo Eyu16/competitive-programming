eac4ea44f49502dfed909ebfdd1b987afdc11fb5
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2024-02-08 11:48:38 +0300

    Time: 41 ms (67.09%), Space: 16.6 MB (89.42%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.py
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -2,12 +2,19 @@ class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
         result1 = []
         result2 = []
-        for num1 in arr2:
-            for num2 in arr1:
-                if num2 == num1 and num2 in arr2:
-                    result1.append(num2)
+        frequency_map = {}
+        for num in arr1:
+            if num in frequency_map:
+                frequency_map[num] += 1
+            else:
+                frequency_map[num] = 1
+        for num in arr2:
+            if num in frequency_map:
+                result1.extend([num] * frequency_map[num])
+
         for num in arr1:
             if num not in arr2:
                 result2.append(num)
+
         return result1 + sorted(result2)
         

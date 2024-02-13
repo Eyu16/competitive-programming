afc13a14165e44e3d7287fa08e49a1a6e20c5995
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2024-02-13 16:44:08 +0300

    Time: 38 ms (97.71%), Space: 16.6 MB (61.47%) - LeetHub

--- 0789-escape-the-ghosts/0789-escape-the-ghosts.py
+++ 0789-escape-the-ghosts/0789-escape-the-ghosts.py
@@ -1,7 +1,10 @@
 class Solution:
     def escapeGhosts(self, ghosts: List[List[int]], target: List[int]) -> bool:
         person_distance = abs(target[0]) + abs(target[1])
-        ghosts_distance = [abs(target[0] - ghost[0]) + abs(target[1] - ghost[1]) for ghost in ghosts]
-        if person_distance < min(ghosts_distance):
-            return True
-        return False
+
+        for ghost in ghosts:
+            ghost_distance = abs(target[0] - ghost[0]) + abs(target[1] - ghost[1])
+            if ghost_distance <= person_distance:
+                return False
+
+        return True

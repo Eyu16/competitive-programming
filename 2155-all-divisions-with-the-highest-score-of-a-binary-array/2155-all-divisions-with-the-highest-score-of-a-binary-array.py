fddfd83dd25e15387c8349b3257570816eb46d2f
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2024-02-23 22:53:03 +0300

    Time: 2585 ms (29.42%), Space: 41.2 MB (30.15%) - LeetHub

--- 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.py
+++ 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.py
@@ -2,38 +2,43 @@ class Solution:
     def maxScoreIndices(self, nums: List[int]) -> List[int]:
         zero_count = 0
         one_count = 0
+        total_zero = nums.count(0)
+        total_one = len(nums) - total_zero
         max_val = 0
         res = []
-        index_count_map = {}
         index_sum_map = {}
 
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                zero_count += 1
-                index_count_map[i] = [zero_count, one_count]
-            else:
-                one_count += 1
-                index_count_map[i] = [zero_count, one_count]
-
-        for i in range(len(nums) + 1):
+        for i in range(len(nums)+ 1):
             if i == 0:
-                sums = index_count_map[len(nums) - 1][1]
-                max_val = max(max_val, sums)
-                index_sum_map[i] = sums
+                if nums[i] == 0:
+                    zero_count += 1
+                    max_val = max(max_val, total_zero)
+                else:
+                    one_count += 1
+                index_sum_map[i] = total_one
+                max_val = max(max_val, total_one)
             elif i == len(nums):
-                sums = index_count_map[i - 1][0]
-                max_val = max(max_val, sums)
-                index_sum_map[i] = sums
+                index_sum_map[i] = total_zero
+                max_val = max(max_val, total_zero)
             else:
-                sums = index_count_map[i-1][0] + index_count_map[len(nums)-1][1] - index_count_map[i - 1][1]
-                max_val = max(max_val, sums)
-                index_sum_map[i] = sums
+                if nums[i] == 0:
+                    sums = zero_count + total_one - one_count
+                    zero_count += 1
+                    max_val = max(max_val, sums)
+                    index_sum_map[i] = sums
+
+                else:
+                    sums = zero_count + total_one - one_count
+                    one_count += 1
+                    max_val = max(max_val, sums)
+                    index_sum_map[i] = sums
 
         for key in index_sum_map:
             if index_sum_map[key] == max_val:
                 res.append(key)
-                
+
         return res
+
 
 
 

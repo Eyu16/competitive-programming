0ed6f47844dabaf293c0c459e25ed76dc8f68bed
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2024-02-27 21:57:46 +0300

    Time: 302 ms (15.11%), Space: 17 MB (85.21%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,17 +1,24 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        # height_map = {}
-        # for i in range(len(names)):
-        #     height_map[heights[i]] = names[i]
-        n = len(names)
-        for i in range(len(heights)):
-            for j in range(n - 1):
-                if heights[j] < heights[j + 1]:
-                    heights[j], heights[j + 1] = heights[j + 1], heights[j]
-                    names[j], names[j + 1] = names[j + 1], names[j]
-            n -= 1
-                    
-                    
+#         n = len(names)
+#         for i in range(len(heights)):
+#             for j in range(n - 1):
+#                 if heights[j] < heights[j + 1]:
+#                     heights[j], heights[j + 1] = heights[j + 1], heights[j]
+#                     names[j], names[j + 1] = names[j + 1], names[j]
+#             n -= 1    
+            
+            
+            
+        for i in range(len(heights) - 1):
+            min_index = i
+            for j in range(i + 1, len(heights)):
+                if heights[min_index] < heights[j]:
+                    min_index = j
+            heights[i],heights[min_index] = heights[min_index], heights[i]
+            names[i], names[min_index] = names[min_index], names[i]
+            
+            
         return names
         
                     

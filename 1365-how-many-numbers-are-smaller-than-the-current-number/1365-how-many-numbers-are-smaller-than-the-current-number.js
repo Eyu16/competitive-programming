ff82871dd424d47bf5c448188fee79776f1ae853
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2024-02-28 02:29:50 +0300

    Time: 70 ms (55.99%), Space: 52 MB (18.58%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
@@ -5,21 +5,6 @@
 var smallerNumbersThanCurrent = function(nums) {
   let max = Math.max(...nums)+1;
     let smaller = new Array(max).fill(0);
-//     for(let i = 0; i<nums.length;i++){
-//         smaller[nums[i]]++;
-//     }
-//     for(let i = 1 ; i < smaller.length ; i++){
-//         smaller[i] += smaller[i-1]
-       
-//     }
-//     for(let i = 0 ; i<nums.length ; i++){
-//         let position = nums[i];
-//         if(position === 0) nums[i] = 0;
-//         else{
-//             nums[i] = smaller[position-1];
-            
-//         }
-//     }
     for(let i = 0 ; i < nums.length ; i++){
         smaller[nums[i]]++;
     }

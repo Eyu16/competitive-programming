7c9d0bfb31e1160db2af36a28fd1039a07cf7b55
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2024-03-26 10:43:47 -0700

    Time: 29 ms (11.54%), Space: 12 MB (12.49%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -1,7 +1,14 @@
-class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        sums = 0
-        for i in range(len(nums)):
-            sums += nums[i]
-            nums[i] = sums
-        return nums
+class Solution(object):
+    def runningSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        prefix_sum = []
+        cum_sum = 0
+        for num in nums:
+            cum_sum += num
+            prefix_sum.append(cum_sum)
+            
+        return prefix_sum
+        

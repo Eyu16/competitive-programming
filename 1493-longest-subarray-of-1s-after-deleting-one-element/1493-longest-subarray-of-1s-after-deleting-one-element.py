8a3cea073a6a832b85c321f7008aee6e26f6c25c
Eyob Yihalem <122790467+Eyu16@users.noreply.github.com>
2024-03-27 02:28:35 -0700

    Time: 445 ms (79.21%), Space: 15 MB (73.24%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -6,32 +6,14 @@ class Solution(object):
         """
         l = 0
         r = 0
-        p = 0
-        leng = 0
-        f_map = defaultdict(int)
-        while r < len(nums):
-            if nums[r] == 1:
-                f_map[1] += 1
-                r += 1
-            else:
-                f_map[0] += 1
-                if f_map[0] == 2:
-                    leng = max(leng, f_map[1])
-                    if nums[p] == 0 and l != p:
-                        f_map[1] -=(l - p - 1)
-                    else:
-                        f_map[1] -= (l - p)
-                    f_map[0] -= 1
-                    p = l
-                    l = r
-                else:
-                    l = r
-                r += 1
+        num_of_zeroes = 1
+        while r < len((nums)):
+            if nums[r] == 0:
+                num_of_zeroes -= 1
+            if num_of_zeroes < 0:
+                if nums[l] == 0:
+                    num_of_zeroes += 1
+                l += 1
+            r += 1
 
-        if l == 0:
-            return len(nums) - 1
-        else:
-            if nums[p] == 0:
-                return max(leng, r - p - 2)
-            else:
-                return max(leng, r - p - 1)
+        return r - l - 1
